b56e7d0656f915be548bac59e71febbcea87b670
Naoyuki Iwamoto <rb.naoyuki@gmail.com>
2017-05-07 09:57:08 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by NAOYUKI IWAMOTO on H29/05/07.
+//  Copyright © 平成29年 naoyuki.iwamoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
