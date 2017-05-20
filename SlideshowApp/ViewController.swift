916c059627d8791cd6599d5677671dc2be3c52e5
Naoyuki Iwamoto <rb.naoyuki@gmail.com>
2017-05-20 18:03:28 +0900

    First Commitment

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -10,16 +10,120 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var imageView: UIImageView!
+    var imageArray = [UIImage]()
+    var timer: Timer? = nil
+    var updateCount = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let image0 = UIImage(named: "0.jpg")
+        imageView.image = image0
+        
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let image0 = UIImage(named: "0.jpg")
+        let image1 = UIImage(named: "1.jpg")
+        let image2 = UIImage(named: "2.jpg")
+        let image3 = UIImage(named: "3.jpg")
+        let image4 = UIImage(named: "4.jpg")
+        
+        imageArray = [image0!, image1!, image2!, image3!, image4!]
+        imageView.image = imageArray[updateCount]
+        
+    }
+    
+    @IBAction func startstopButton(_ sender: Any) {
+        
+        
+        if (timer == nil) {
+            
+            timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(updateImage), userInfo: nil, repeats: true)
+            
+        } else {
+            
+            timer?.invalidate()
+            timer = nil
+        }
+    }
+    
+    
+    @IBAction func nextButton(_ sender: Any) {
+        
+        if (timer == nil) {
+            
+            if (updateCount == 4) {
+                
+                updateCount = 0
+                imageView.image = imageArray[updateCount]
+                
+            } else {
+            
+                updateCount = updateCount + 1
+                imageView.image = imageArray[updateCount]
+                
+            }
+            
+        } else {
+            
+            
+        }
+    
+    }
+    
+    @IBAction func backButton(_ sender: Any) {
+        
+        if (timer == nil) {
+            
+            if (updateCount == 0) {
+                
+                updateCount = 4
+                imageView.image = imageArray[updateCount]
+            } else {
+                
+                updateCount = updateCount - 1
+                imageView.image = imageArray[updateCount]
+            }
+        } else {
+            
+        }
+    }
+    
+    func updateImage() {
+        
+        if(updateCount == 4) {
+            
+            updateCount = 0
+            imageView.image = imageArray[updateCount]
+            
+        } else {
+
+            updateCount = updateCount + 1
+            imageView.image = imageArray[updateCount]
+        }
+        
+
+    }
+    
+    @IBAction func nextPage(_ sender: Any) {
+        
+        
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        let extendViewController:extendViewController = segue.destination as! extendViewController
+        extendViewController.updateCount = updateCount
+        
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
-
